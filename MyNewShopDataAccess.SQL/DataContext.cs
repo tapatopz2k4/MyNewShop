fbdae6933460dfd08692304dedf1a2d742edd00b
tapatopz2k4 <39274060+tapatopz2k4@users.noreply.github.com>
2018-06-07 11:59:09 -0500

    EFDatabaseCreation

---
+++ MyNewShopDataAccess.SQL/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyNewShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewShopDataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //The (base) below allows us to pass in the Connection String sname information.
+        public DataContext()
+            : base("EFTestDB_dev") {
+
+        }
+
+
+        //These declarations with (DbSet) specifies for EF which models will be storing tables in the Database.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories {get; set; }
+
+
+
+    }
+}
