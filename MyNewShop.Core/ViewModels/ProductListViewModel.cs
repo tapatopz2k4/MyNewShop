7a5f40a6c40d6266f2d5e38ca4dd3d055b82a5c0
tapatopz2k4 <39274060+tapatopz2k4@users.noreply.github.com>
2018-06-08 11:30:07 -0500

    Added Product Filters

---
+++ MyNewShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyNewShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+
+        public IEnumerable<Product> Products { set; get; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
--- MyNewShop.WebUI/Controllers/HomeController.cs
+++ MyNewShop.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyNewShop.Core.ViewModels;
 
 namespace MyNewShop.WebUI.Controllers
 {
@@ -22,10 +23,27 @@ namespace MyNewShop.WebUI.Controllers
         }
 
 
-        public ActionResult Index()
+        public ActionResult Index(string Category = null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+
+
+            return View(model);
         }
 
 
